4f70bc31452217fc35003a4a9d8b842696eff54f
Cirrus CI <support@cirruslabs.org>
2020-09-10 23:08:23 +0000

    Atualizacoes de telas

--- lib/components/category_item.dart
+++ lib/components/category_item.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutterCozinhar/models/category.dart';
+import '../screens/categories_meals_screen.dart';
 
 class CategoryItem extends StatelessWidget {
 
@@ -7,10 +8,38 @@ class CategoryItem extends StatelessWidget {
 
   CategoryItem(this.category);
 
+  void _selectCategory(BuildContext context) {
+    Navigator.of(context).push(MaterialPageRoute(
+      builder: (_) {
+        return CategoriesMealsScreen();
+      },
+      ),);
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text(category.title),    
+    return InkWell(
+      onTap: () => _selectCategory(context),
+      splashColor: Theme.of(context).primaryColor,
+      borderRadius: BorderRadius.circular(15),
+          child: Container(
+        child: Text(
+          category.title,
+          style: Theme.of(context).textTheme.title,
+          ),   
+        padding: EdgeInsets.all(15),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(15),
+          gradient: LinearGradient(
+            colors: [
+              category.color.withOpacity(0.5),
+              category.color,
+            ],
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+          ),
+        ), 
+      ),
     );
   }
 }
